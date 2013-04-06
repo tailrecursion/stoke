f9e46a807e1aa50d4248aedd2376c95b52ba8345
Micha Niskin <asdf@ubergibson.com>
2013-04-06 02:02:18 -0400

    oops

--- src/tailrecursion/stoke/mode/normal.clj
+++ src/tailrecursion/stoke/mode/normal.clj
@@ -17,11 +17,13 @@
   (swap! mult #(let [i (Character/digit c 10)] 
                  (if (not %) i (+ (* 10 %) i)))))
 
-(defmethod normal-cmd \q [_] ::quit)
+(defmethod normal-cmd \q [_] :tailrecursion.stoke.term/quit)
 (defmethod normal-cmd \h [_] (edit zip/left))
 (defmethod normal-cmd \H [_] (edit zip/leftmost))
 (defmethod normal-cmd \l [_] (edit zip/right))
 (defmethod normal-cmd \L [_] (edit zip/rightmost))
 (defmethod normal-cmd \j [_] (edit zip/down))
 (defmethod normal-cmd \k [_] (edit zip/up))
+(defmethod normal-cmd \n [_] (edit zip/next))
+(defmethod normal-cmd \p [_] (edit zip/prev))
 (defmethod normal-cmd \e [_] (e/read-file (str (read))))
