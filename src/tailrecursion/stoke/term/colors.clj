ff5d4d3b13c1ddf3458e25422aa675910179b1ec
Micha Niskin <asdf@ubergibson.com>
2013-04-08 13:25:14 -0400

    forgot file

---
+++ src/tailrecursion/stoke/term/colors.clj
@@ -0,0 +1,11 @@
+(ns tailrecursion.stoke.term.colors)
+
+(def colors
+  {:default   {:color 103 :bold 0}
+   :point     {:color 214 :bold 1}
+   :keyword   {:color 107 :bold 0}
+   :symbol    {:color 103 :bold 0}
+   :clj-core  {:color 103 :bold 1}
+   :java-cls  {:color 203 :bold 1}
+   :string    {:color  14 :bold 0}})
+
